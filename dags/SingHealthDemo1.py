7c668ac2cdffac385840f112f3c5f11933c02087
Mike <ozzyathome@gmail.com>
2020-12-16 14:04:43 +0000

    general update

--- dags/SingHealthDemo1.py
+++ dags/SingHealthDemo1.py
@@ -9,9 +9,10 @@ from datetime import datetime, timedelta
 def  convert_csv_func():
     print('Fixup CSV to parquet')
     import pandas
-    df=pandas.read_csv("/usr/local/input/singhealth_dataset.csv",header=None)
-    df.columns=["first_name","last_name","email","dob","id","gender","colour","religion","civil_stand","number"]
-    df.to_csv("/usr/local/input/singhealth_dataset1.csv")
+    #df=pandas.read_csv("/usr/local/input/singhealth_dataset.csv",header=None)
+    df=pandas.read_csv("/usr/local/input/singhealth_dataset.csv",header=0)
+    #df.columns=["NRIC,first_name","last_name","email","dob","id","gender","colour","religion","civil_stand","number"]
+    #df.to_csv("/usr/local/input/singhealth_dataset1.csv")
     df.to_parquet("/usr/local/input/singhealth_dataset1.parquet")
     return 'completed csv fixup'
 
@@ -26,7 +27,7 @@ def  export_csv_func():
 
 with DAG(dag_id='singhealth_dag', schedule_interval="@once", start_date=datetime(2020, 1, 1), catchup=False) as dag:
 
-    check_input = BashOperator(task_id='check_csv_input', bash_command='ls -l /usr/local/input')
+    check_input = BashOperator(task_id='check_csv_input', bash_command='id')
 
     handle_csv = PythonOperator(task_id='convert_csv', python_callable=convert_csv_func)
 
@@ -36,12 +37,14 @@ with DAG(dag_id='singhealth_dag', schedule_interval="@once", start_date=datetime
 
     export_csv= PythonOperator(task_id='export_csv', python_callable=export_csv_func)
 
+
     tokenize_csv= DockerOperator(
         task_id='docker_command',
-        image='hello-world',
+        image='res-drl-docker-local.artifactory.swg-devops.com/dpt/base-cli',
         api_version='auto',
         auto_remove=True,
         command="",
+        volumes=['/usr/local/input/:/input','/usr/local/input/:/config','/usr/local/output/:/output'], 
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge"
         )
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+ExecStartPre=/sbin/setcap 'cap_ipc_lock=+ep' $(readlink -f $(which vault))
 export MYUID=1001
 export MYGID=1001
 docker-compose -f docker-compose-LocalExecutor.yml up
